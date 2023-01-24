7f70bba1f8df1e0199e97c9869ccd45adc550585
Darren Lou <darren.lou@gmail.com>
2023-01-24 10:26:51 -0800

    Add create food entry functionality to app.js
    * add mealItemObj and data to be passed down
    * add weather api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import Meal from './components/meal';
 
+const getWeatherKey = 'dec6459938d398a140c7e3eedf4f4959';
+
+let lon;
+let lat;
+
+navigator.geolocation.getCurrentPosition((position) => {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+});
+
+console.log(lat, lon);
+
+const getWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${getWeatherKey}`;
+console.log(getWeatherUrl);
+
+fetch(getWeatherUrl).then((response) => console.log(response));
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,17 +26,12 @@ class App extends Component {
 
     this.state = {
       today: today,
+      foodItemIdCounter: 0,
+      foodItem: '',
       message: 'To Do List',
       navList: ['Todo List', 'Calculator', 'Weather Forecast'],
       mealEntryIdCounter: 0,
-      meals: [
-        { meal1: [{ name: 'pizza' }, { name: 'pizza' }] },
-        { meal2: [{ name: 'pineapple' }, { name: 'pizza' }] },
-        { meal3: [{ name: 'pizza' }, { name: 'pizza' }] },
-        { meal4: [{ name: 'spaghetti' }, { name: 'pizza' }] },
-        { meal5: [{ name: 'pizza' }, { name: 'pizza' }] },
-        { meal6: [{ name: 'pizza' }, { name: 'pizza' }] },
-      ],
+      mealList: [],
     };
   }
 
@@ -28,6 +40,55 @@ class App extends Component {
     //   todoItem: '',
     // });
     console.log('component did mount');
+  }
+
+  handleAddFood(e) {
+    e.preventDefault();
+
+    const mealItemId = this.state.todoItemIdCounter + 1;
+
+    const mealItemObj = {
+      id: mealItemId,
+      name: e.target.mealItem.value,
+      completed: false,
+      nutrition: {
+        calories: 23,
+        protein: 0,
+        carbohydrates: {
+          total: 0,
+          fiber: 0,
+          sugar: 0,
+        },
+        fats: {
+          total: 0,
+          saturated: 0,
+          polyunsaturated: 0,
+          monounsaturated: 0,
+          trans: 0,
+        },
+        cholesterol: 0,
+        sodium: 0,
+        potassium: 0,
+        vitaminA: 0,
+        vitaminC: 0,
+        calcium: 0,
+        iron: 0,
+      },
+      brand: {
+        name: 'tyson',
+      },
+      description: '',
+      serving: {
+        size: {},
+      },
+      buy: {},
+    };
+
+    this.setState({
+      todoItem: '',
+      todoItemIdCounter: mealItemId,
+      todoList: [...this.state.meals, mealItemObj],
+    });
   }
 
   render() {
--- src/components/meal.js
+++ src/components/meal.js
@@ -1,44 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AddFood from './addFood';
 
-const Meal = (props) => (
-  <div className='meal-container'>
-    <table className='meal-table table table-striped'>
-      <thead>
-        <tr>
-          <th scope='col'>Food Item</th>
-          <th scope='col'>P</th>
-          <th scope='col'>C</th>
-          <th scope='col'>F</th>
-          <th scope='col'>Cals</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope='row'>{props.id}</th>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{400}</td>
-        </tr>
-        <tr>
-          <th scope='row'>Chicken Breast - Baked</th>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{400}</td>
-        </tr>
-        <tr>
-          <th scope='row'>Jasmine Rice</th>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{`40g`}</td>
-          <td>{200}</td>
-        </tr>
-      </tbody>
-    </table>
-    <AddFood />
-  </div>
-);
+class Meal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { meal: props.meal };
+  }
+
+  componentDidMount() {
+    this.setState({});
+  }
+
+  render() {
+    return (
+      <div className='meal-container'>
+        <table className='meal-table table table-striped'>
+          <thead>
+            <tr>
+              <th scope='col'>Food Item</th>
+              <th scope='col'>P</th>
+              <th scope='col'>C</th>
+              <th scope='col'>F</th>
+              <th scope='col'>Cals</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope='row'>{this.props.meal}</th>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{400}</td>
+            </tr>
+            <tr>
+              <th scope='row'>Chicken Breast - Baked</th>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{400}</td>
+            </tr>
+            <tr>
+              <th scope='row'>Jasmine Rice</th>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{`40g`}</td>
+              <td>{200}</td>
+            </tr>
+          </tbody>
+        </table>
+        <AddFood />
+      </div>
+    );
+  }
+}
 
 export default Meal;
